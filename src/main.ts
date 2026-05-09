b43753fe5e89090927ccbce93084f1ce719c9940
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 23:14:01 +0200

    Load timer ring progress fix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import './advanced-options.css'
 import './theme-polish.css'
 import './configurator-v2.css'
 import './tv-final-polish.css'
+import './timer-ring-fix.css'
 import App from './AppOpenWeather.svelte'
 
 const app = mount(App, {
