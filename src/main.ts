500fe8aa4823289873a620c73185e97966ddb8e8
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 19:50:31 +0200

    Use simplified autosaving configurator

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import './theme-radical.css'
 import './section-animations.css'
 import './advanced-options.css'
 import './theme-polish.css'
-import App from './AppAdvanced2.svelte'
+import './configurator-v2.css'
+import App from './AppAdvanced3.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
