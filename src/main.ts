17fe520af88a32efe4f192be55a8d2dd775f99e8
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 19:24:59 +0200

    Use advanced TiempoTV app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { mount } from 'svelte'
 import './app.css'
 import './theme-radical.css'
 import './section-animations.css'
-import App from './App.svelte'
+import './advanced-options.css'
+import App from './AppAdvanced.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
