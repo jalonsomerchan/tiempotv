665fcc2946787c16ea97966ec0a22d596f927e6d
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 22:33:43 +0200

    Use fixed reactive weather app

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import './section-animations.css'
 import './advanced-options.css'
 import './theme-polish.css'
 import './configurator-v2.css'
-import App from './AppStable.svelte'
+import App from './AppFixed.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
