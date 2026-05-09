93059e0b4802884a8cb3d9a18d436be897c42faa
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 22:58:41 +0200

    Load WMO icons app and CSS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import './section-animations.css'
 import './advanced-options.css'
 import './theme-polish.css'
 import './configurator-v2.css'
-import App from './AppClean.svelte'
+import './wmo-icons.css'
+import App from './AppWmo.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
