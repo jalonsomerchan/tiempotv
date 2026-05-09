5aa5755ea751498f7ad156588a8854714d8075a8
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 22:51:00 +0200

    Use clean weather app with reliable location selection

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import './section-animations.css'
 import './advanced-options.css'
 import './theme-polish.css'
 import './configurator-v2.css'
-import App from './AppFixed.svelte'
+import App from './AppClean.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
