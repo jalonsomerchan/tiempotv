05bf48664a7397f5ffb7074cbe1c079b51b171c0
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 22:19:08 +0200

    Use municipality refresh fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import './section-animations.css'
 import './advanced-options.css'
 import './theme-polish.css'
 import './configurator-v2.css'
-import App from './AppAdvanced3.svelte'
+import App from './AppAdvanced4.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
