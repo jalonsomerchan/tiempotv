1623f7113e59f1f95238ca5866e6524caf526501
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-10 09:55:37 +0200

    Route channel to stable standalone renderer

---
+++ src/AppRouter.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import ChannelStable from './ChannelStable.svelte'
+  import AppWeatherResilient from './AppWeatherResilient.svelte'
+
+  const appBase = import.meta.env.BASE_URL.replace(/\/$/, '')
+  const cleanPath = typeof window !== 'undefined'
+    ? window.location.pathname.replace(new RegExp(`^${appBase}`), '')
+    : ''
+  const isChannel = cleanPath.startsWith('/canal')
+</script>
+
+{#if isChannel}
+  <ChannelStable />
+{:else}
+  <AppWeatherResilient />
+{/if}
